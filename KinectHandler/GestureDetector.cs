d60fddf1f89ba4b061f9a6a30df0ca304539561a
Fernando González López - Peñalver <aladaris@gmail.com>
2015-05-30 19:56:03 +0100

    Creada la librería KinectHandler, con los manejadores necesarios para lanzar los gestos desde Kinect. Se ha implementado un sistema por le cual, u ngesto detectado debe permanecer detectado durante N (de momento 200) milisegundos, para que se lance la acción correpsondiente.

---
+++ KinectHandler/GestureDetector.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectHandler {
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.VisualGestureBuilder;
+
+    public delegate void DiscreteGestureDetected(string gestureName, ulong bodyTrackingId, bool isGestureDetected, float detectionConfidence);
+
+    public class Gesto {
+        private bool _detected;
+        public string Name { get; private set; }
+        public string DataBase { get; private set; }
+        public bool Detected {
+            get { return _detected; }
+            set {
+                PreviousDetected = _detected;
+                _detected = value;
+            }
+        }
+        public bool PreviousDetected { get; private set; }
+
+        public Gesto(string name, string database) {
+            Name = name;
+            DataBase = database;
+            _detected = false;
+            PreviousDetected = false;
+        }
+    }
+
+    /// <summary>
+    /// Gesture Detector class which listens for VisualGestureBuilderFrame events from the service
+    /// and updates the associated GestureResultView object with the latest results for the 'Seated' gesture
+    /// </summary>
+    public class GestureDetector : IDisposable {
+        private readonly Gesto[] _gestos = {
+                                               new Gesto("Seated", @"Database\Seated.gbd"),
+                                               new Gesto("Arm_T_Right", @"Database\Arm_T_Right.gba")
+                                           };
+
+        /// <summary> Gesture frame source which should be tied to a body tracking ID </summary>
+        private VisualGestureBuilderFrameSource vgbFrameSource = null;
+        /// <summary> Gesture frame reader which will handle gesture events coming from the sensor </summary>
+        private VisualGestureBuilderFrameReader vgbFrameReader = null;
+
+        // events
+        public event DiscreteGestureDetected GestureDetectedChanged;
+
+
+        /// <summary>
+        /// Initializes a new instance of the GestureDetector class along with the gesture frame source and reader
+        /// </summary>
+        /// <param name="kinectSensor">Active sensor to initialize the VisualGestureBuilderFrameSource object with</param>
+        /// <param name="gestureResultView">GestureResultView object to store gesture results of a single body to</param>
+        public GestureDetector(KinectSensor kinectSensor) {
+            if (kinectSensor == null) {
+                throw new ArgumentNullException("kinectSensor");
+            }
+
+            // create the vgb source. The associated body tracking ID will be set when a valid body frame arrives from the sensor.
+            this.vgbFrameSource = new VisualGestureBuilderFrameSource(kinectSensor, 0);
+            this.vgbFrameSource.TrackingIdLost += this.Source_TrackingIdLost;
+
+            // open the reader for the vgb frames
+            this.vgbFrameReader = this.vgbFrameSource.OpenReader();
+            if (this.vgbFrameReader != null) {
+                this.vgbFrameReader.IsPaused = true;
+                this.vgbFrameReader.FrameArrived += this.Reader_GestureFrameArrived;
+            }
+
+            // load the gestures from the gesture databases
+            for (int i = 0; i < _gestos.Length; i++) {
+                using (VisualGestureBuilderDatabase database = new VisualGestureBuilderDatabase(_gestos[i].DataBase)) {
+                    // we could load all available gestures in the database with a call to vgbFrameSource.AddGestures(database.AvailableGestures), 
+                    // but for this program, we only want to track one discrete gesture from the database, so we'll load it by name
+                    foreach (Gesture gesture in database.AvailableGestures) {
+                        if (gesture.Name.Equals(_gestos[i].Name)) {
+                            this.vgbFrameSource.AddGesture(gesture);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the body tracking ID associated with the current detector.
+        /// The tracking ID can change whenever a body comes in/out of scope.
+        /// </summary>
+        public ulong TrackingId {
+            get {
+                return this.vgbFrameSource.TrackingId;
+            }
+
+            set {
+                if (this.vgbFrameSource.TrackingId != value) {
+                    this.vgbFrameSource.TrackingId = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether or not the detector is currently paused
+        /// If the body tracking ID associated with the detector is not valid, then the detector should be paused
+        /// </summary>
+        public bool IsPaused {
+            get {
+                return this.vgbFrameReader.IsPaused;
+            }
+
+            set {
+                if (this.vgbFrameReader.IsPaused != value) {
+                    this.vgbFrameReader.IsPaused = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disposes all unmanaged resources for the class
+        /// </summary>
+        public void Dispose() {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes the VisualGestureBuilderFrameSource and VisualGestureBuilderFrameReader objects
+        /// </summary>
+        /// <param name="disposing">True if Dispose was called directly, false if the GC handles the disposing</param>
+        protected virtual void Dispose(bool disposing) {
+            if (disposing) {
+                if (this.vgbFrameReader != null) {
+                    this.vgbFrameReader.FrameArrived -= this.Reader_GestureFrameArrived;
+                    this.vgbFrameReader.Dispose();
+                    this.vgbFrameReader = null;
+                }
+
+                if (this.vgbFrameSource != null) {
+                    this.vgbFrameSource.TrackingIdLost -= this.Source_TrackingIdLost;
+                    this.vgbFrameSource.Dispose();
+                    this.vgbFrameSource = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles gesture detection results arriving from the sensor for the associated body tracking Id
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_GestureFrameArrived(object sender, VisualGestureBuilderFrameArrivedEventArgs e) {
+            VisualGestureBuilderFrameReference frameReference = e.FrameReference;
+            using (VisualGestureBuilderFrame frame = frameReference.AcquireFrame()) {
+                if (frame != null) {
+                    // get the discrete gesture results which arrived with the latest frame
+                    IReadOnlyDictionary<Gesture, DiscreteGestureResult> discreteResults = frame.DiscreteGestureResults;
+
+                    if (discreteResults != null) {
+                        foreach (Gesture gesture in this.vgbFrameSource.Gestures) {
+                            for (int i = 0; i < _gestos.Length; i++) {
+                                if (gesture.Name.Equals(_gestos[i].Name) && gesture.GestureType == GestureType.Discrete) {
+                                    DiscreteGestureResult result = null;
+                                    discreteResults.TryGetValue(gesture, out result);
+                                    if (result != null) {
+                                        // AQUÍ VA EL CÓDIGO LANZADO POR EL GESTO 'gesture' con resultado 'result'
+                                        _gestos[i].Detected = result.Detected;
+                                        if (_gestos[i].Detected != _gestos[i].PreviousDetected) {
+                                            GestureDetectedChanged(_gestos[i].Name, TrackingId, result.Detected, result.Confidence);
+                                        }
+                                        //System.Windows.Forms.MessageBox.Show(result.Detected.ToString() + " gesture: " + gesture.Name + " q: " + result.Confidence.ToString());  // DEBUG
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the TrackingIdLost event for the VisualGestureBuilderSource object
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Source_TrackingIdLost(object sender, TrackingIdLostEventArgs e) {
+            // TODO: Algo?
+        }
+    }
+}
---
+++ KinectHandler/KinectHanlder.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Microsoft.Kinect.VisualGestureBuilder;
+using System.Timers;
+
+namespace KinectHandler {
+
+    public enum INTERACTION { NONE, FLARE, GLOW, TIMETRAVEL, SPECTRUM }
+    public delegate void SmoothedInput (ulong trackingId, INTERACTION interaction);
+
+    class Player : IDisposable {
+        public ulong TrackingId { get; set; }
+        public INTERACTION Interaction { get; private set; }
+        public Timer SmoothTimer { get; private set; }
+        public event SmoothedInput OnSmoothedGesture;
+        public bool Detected { get; private set; }
+
+        public Player(ulong trackingId, INTERACTION interaction = INTERACTION.NONE, double smooth_time = 200) {
+            TrackingId = trackingId;
+            Interaction = interaction;
+            SmoothTimer = new Timer(smooth_time);
+            SmoothTimer.Elapsed += SmoothTimerElapsed;
+        }
+
+        public void SmoothInput(INTERACTION interaction, bool detected) {
+            if (SmoothTimer != null) {
+                // Comenzamos el timer
+                if (!SmoothTimer.Enabled) {
+                    Interaction = interaction;
+                    SmoothTimer.Start();
+                // Si el timer ya estaba funcionando, paramos todo el proceso de detección.
+                } else {
+                    if (Detected != detected) {
+                        SmoothTimer.Stop();
+                        Interaction = INTERACTION.NONE;
+                    }
+                }
+                Detected = detected;
+            }
+        }
+
+        public void Dispose() {
+            if (SmoothTimer != null) {
+                SmoothTimer.Stop();
+                SmoothTimer.Dispose();
+                TrackingId = 0;
+                Interaction = INTERACTION.NONE;
+            }
+        }
+
+        private void SmoothTimerElapsed(object sender, ElapsedEventArgs e) {
+            if (OnSmoothedGesture != null) {
+                OnSmoothedGesture(TrackingId, Interaction);
+                SmoothTimer.Stop();
+            }
+        }
+    }
+
+    class KinectHanlder : IDisposable {
+        /// <summary> Active Kinect sensor </summary>
+        private KinectSensor kinectSensor = null;
+        /// <summary> Array for the bodies (Kinect will track up to 6 people simultaneously) </summary>
+        private Body[] bodies = null;
+        /// <summary> Reader for body frames </summary>
+        private BodyFrameReader bodyFrameReader = null;
+        /// <summary> List of gesture detectors, there will be one detector created for each potential body (max of 6) </summary>
+        private List<GestureDetector> gestureDetectorList = null;
+        private List<Player> _players = null;
+
+        public KinectHanlder() {
+            // only one sensor is currently supported
+            this.kinectSensor = KinectSensor.GetDefault();
+            // set IsAvailableChanged event notifier
+            this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
+            // open the sensor
+            this.kinectSensor.Open();
+            // open the reader for the body frames
+            this.bodyFrameReader = this.kinectSensor.BodyFrameSource.OpenReader();
+            // set the BodyFramedArrived event notifier
+            this.bodyFrameReader.FrameArrived += this.Reader_BodyFrameArrived;
+            // initialize the gesture detection objects for our gestures
+            this.gestureDetectorList = new List<GestureDetector>();
+
+            _players = new List<Player>();
+
+            // create a gesture detector for each body (6 bodies => 6 detectors)
+            int maxBodies = this.kinectSensor.BodyFrameSource.BodyCount;
+            for (int i = 0; i < maxBodies; ++i) {
+                GestureDetector detector = new GestureDetector(this.kinectSensor);
+                detector.GestureDetectedChanged += GestureChanged;
+                this.gestureDetectorList.Add(detector);
+                Player p = new Player((ulong)i);
+                p.OnSmoothedGesture += SmoothedGesture;
+                _players.Add(p);
+            }
+        }
+
+        private void SmoothedGesture(ulong trackingId, INTERACTION interaction) {
+            Player p = GetPlayer(trackingId);
+            if (p != null) {
+                if (p.Detected) {
+                    // TODO: AQUÍ LANZAMOS EL GESTO
+                }
+            }
+        }
+
+        public void Dispose() {
+            if (this.bodyFrameReader != null) {
+                // BodyFrameReader is IDisposable
+                this.bodyFrameReader.FrameArrived -= this.Reader_BodyFrameArrived;
+                this.bodyFrameReader.Dispose();
+                this.bodyFrameReader = null;
+            }
+            if (this.gestureDetectorList != null) {
+                // The GestureDetector contains disposable members (VisualGestureBuilderFrameSource and VisualGestureBuilderFrameReader)
+                foreach (GestureDetector detector in this.gestureDetectorList) {
+                    detector.Dispose();
+                }
+
+                this.gestureDetectorList.Clear();
+                this.gestureDetectorList = null;
+            }
+            if (this.kinectSensor != null) {
+                this.kinectSensor.IsAvailableChanged -= this.Sensor_IsAvailableChanged;
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+            if (_players != null){
+                foreach (Player p in _players){
+                    p.Dispose();
+                }
+                _players.Clear();
+            }
+        }
+
+        private void GestureChanged(string gestureName, ulong trackingId, bool isGestureDetected, float detectionConfidence) {
+            Player p = GetPlayer(trackingId);
+            if (p != null) {
+                INTERACTION interaction = INTERACTION.NONE;  // TODO: Poner en función de 'gestureName'
+                p.SmoothInput(interaction, isGestureDetected);
+            }
+        }
+
+        /// <summary>
+        /// Handles the event when the sensor becomes unavailable (e.g. paused, closed, unplugged).
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e) {
+            // TODO: Usar para dar estabilidad!
+        }
+
+        /// <summary>
+        /// Handles the body frame data arriving from the sensor and updates the associated gesture detector object for each body
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_BodyFrameArrived(object sender, BodyFrameArrivedEventArgs e) {
+            bool dataReceived = false;
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame()) {
+                if (bodyFrame != null) {
+                    if (this.bodies == null) {
+                        // creates an array of 6 bodies, which is the max number of bodies that Kinect can track simultaneously
+                        this.bodies = new Body[bodyFrame.BodyCount];
+                    }
+                    // The first time GetAndRefreshBodyData is called, Kinect will allocate each Body in the array.
+                    // As long as those body objects are not disposed and not set to null in the array,
+                    // those body objects will be re-used.
+                    bodyFrame.GetAndRefreshBodyData(this.bodies);
+                    dataReceived = true;
+                }
+            }
+            if (dataReceived) {
+                // we may have lost/acquired bodies, so update the corresponding gesture detectors
+                if (this.bodies != null) {
+                    // loop through all bodies to see if any of the gesture detectors need to be updated
+                    int maxBodies = this.kinectSensor.BodyFrameSource.BodyCount;
+                    for (int i = 0; i < maxBodies; ++i) {
+                        Body body = this.bodies[i];
+                        ulong trackingId = body.TrackingId;
+                        // if the current body TrackingId changed, update the corresponding gesture detector with the new value
+                        if (trackingId != this.gestureDetectorList[i].TrackingId) {
+                            this.gestureDetectorList[i].TrackingId = trackingId;
+                            // if the current body is tracked, unpause its detector to get VisualGestureBuilderFrameArrived events
+                            // if the current body is not tracked, pause its detector so we don't waste resources trying to get invalid gesture results
+                            this.gestureDetectorList[i].IsPaused = trackingId == 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        private Player GetPlayer(ulong trackingId) {
+            if (_players != null) {
+                foreach (Player p in _players) {
+                    if (p.TrackingId == trackingId)
+                        return p;
+                }
+            }
+            return null;
+        }
+    }
+}
